f5009b2db9602a6ad211886150f7aced47899191
ruzannakaramyan <114310412+ruzannakaramyan@users.noreply.github.com>
2022-11-14 16:17:44 +0400

    Create testing.js

---
+++ testing.js
@@ -0,0 +1,7 @@
+const {factorial,  gcd, hcf, toHexadecimal}  = require("./functions.js");
+
+
+console.log(factorial(4));
+console.log(toHexadecimal(45));
+console.log(hcf(12,9));
+console.log(gcd(12,9));
